f9fcdccbd002a64abbe22bb81401db21b77976bc
SaurabhNandeep <110117928+SaurabhNandeep@users.noreply.github.com>
2022-08-08 11:01:53 +0530

    My New Project
    Hello everyone our new project is ready

---
+++ MyProject/src/main/java/pagelayer/Page.java
@@ -0,0 +1,19 @@
+package pagelayer;
+
+public class Page {
+	
+	public static void main(String [] args)
+	{
+		int num=5;
+		int fact=1;
+		
+		for(int i=1; i<=num; i++)
+		{
+			fact=fact*1;
+			
+		}
+		
+		System.err.println("Factorial of"+num+" "+fact);
+	}
+
+}
